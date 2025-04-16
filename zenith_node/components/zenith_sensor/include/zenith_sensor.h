7fcdf913aca2b7f8d9a0009251bc4f102e6486ef
Øistein Kjos <oisteink@users.noreply.github.com>
2025-04-16 19:58:02 +0200

    Linked list

--- zenith_node/components/zenith_sensor/include/zenith_sensor.h
+++ zenith_node/components/zenith_sensor/include/zenith_sensor.h
@@ -6,6 +6,7 @@
 typedef struct zenith_sensor_s zenith_sensor_t;
 typedef zenith_sensor_t *zenith_sensor_handle_t;
 
+
 struct zenith_sensor_s {
     esp_err_t ( *initialize )( zenith_sensor_t *sensor ); // Called to initialize the sensor
     esp_err_t ( *read_temperature )( zenith_sensor_t *sensor, float *out_temp ); 
@@ -26,9 +27,36 @@ struct zenith_sensor_humidity_s {
     float humidity;
 };
 
+typedef enum zenith_sensor_datatype_e {
+    ZENITH_SENSOR_DATATYPE_TEMPERATURE,
+    ZENITH_SENSOR_DATATYPE_HUMIDITY,
+    ZENITH_SENSOR_DATATYPE_PRESSURE,
+    ZENITH_SENSOR_DATATYPE_MAX
+} zenith_sensor_datatype_t;
 
+typedef struct zenith_sensor_datapoint_s {
+    zenith_sensor_datatype_t data_type;
+    union {
+        float data;
+    }
+} zenith_sensor_datapoint_t;
+
+typedef struct zenith_sensor_datapoint_node_s zenith_sensor_datapoint_node_t;
+
+struct zenith_sensor_datapoint_node_s {
+    zenith_sensor_datapoint_t datapoint;
+    zenith_sensor_datapoint_node_t *next;
+};
+
+typedef struct zenith_sensor_data_s {
+    uint8_t number_of_datapoints;
+    zenith_sensor_datapoint_node_t *datapoint_nodes;
+} zenith_sensor_data_t;
+
+esp_err_t zenith_sensor_data_add_datapoint( zenith_sensor_data_t *sensor_data, zenith_sensor_datapoint_t datapoint );
 
 esp_err_t zentih_sensor_init( zenith_sensor_handle_t sensor );
+esp_err_t zenith_sensor_read_data( zenith_sensor_handle_t sensor, zenith_sensor_data_t *data );
 esp_err_t zenith_sensor_read_temperature( zenith_sensor_handle_t sensor, float *out_temp );
 esp_err_t zenith_sensor_read_humidity( zenith_sensor_handle_t sensor, float *out_humidity );
 esp_err_t zenith_sensor_read_pressure( zenith_sensor_handle_t sensor, float *out_pressure );
--- zenith_node/components/zenith_sensor/zenith_sensor.c
+++ zenith_node/components/zenith_sensor/zenith_sensor.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "esp_check.h"
+#include "string.h"
 #include "zenith_sensor.h"
 
 static const char *TAG = "zenith_sensor";
@@ -16,7 +17,34 @@ esp_err_t zentih_sensor_init( zenith_sensor_handle_t sensor ) {
     return ret;
 }
 
+// my head is exploding! 
+esp_err_t zenith_sensor_data_add_datapoint( zenith_sensor_data_t *sensor_data, zenith_sensor_datapoint_t datapoint ) { // Pass sensor_data by reference
+    if ( !sensor_data ) 
+        return ESP_ERR_INVALID_ARG;
 
+    zenith_sensor_datapoint_node_t **tail = &sensor_data->datapoint_nodes; // point tail at the pointer for data_nodes
+    while ( *tail ) // As long as what it points to isn't NULL
+        tail = &(*tail)->next; // Dereference tail, and point to a pointer (&) to it's next (pointer)
+
+    *tail = calloc(1, sizeof( zenith_sensor_datapoint_node_t ) ); // We've reached the first pointer that points to NULL, so we add the new node here.
+    if ( !*tail )
+        return ESP_ERR_NO_MEM;
+
+    (*tail)->datapoint = datapoint; // Copy the data over
+    sensor_data->number_of_datapoints++; // Increase the count. 
+
+    return ESP_OK;          
+}
+
+
+/* esp_err_t zenith_sensor_read_data( zenith_sensor_handle_t sensor, zenith_sensor_data_t *sensor_data ) {
+    esp_err_t ret = ESP_OK;
+    zenith_sensor_data_t *data = calloc( 1, sizeof( zenith_sensor_data_t ) );
+
+    sensor_data = data;
+    return ret;
+}
+ */
 esp_err_t zenith_sensor_read_temperature( zenith_sensor_handle_t sensor, float *out_temp ) {
     esp_err_t ret = ESP_OK;
     ESP_RETURN_ON_FALSE(
